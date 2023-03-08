84b42b043418e8ca24f4629083faac4173480606
Alexashka-2022 <avibus.bonis.2022@gmail.com>
2023-03-08 10:33:29 +0300

    Исправлены замечания код-ревью

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,14 +4,13 @@ export default class PopupWithForm extends Popup {
     constructor(popupSelector, submitForm) {
         super(popupSelector);
         this._popupForm = this._popup.querySelector('.popup__form');
-        this._submitForm = submitForm;
+        this._inputList = this._popupForm.querySelectorAll('.popup__input'); 
+        this._submitForm = submitForm; 
     }
 
     _getInputValues() {
         this._formValues = {};
         
-        this._inputList = this._popupForm.querySelectorAll('.popup__input'); 
-
         this._inputList.forEach(inputElement => {
             this._formValues[inputElement.name] = inputElement.value;
         });
